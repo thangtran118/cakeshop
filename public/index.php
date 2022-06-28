2e8b9f27ca0a91673b815274636291b0948fc65f
thangtran118 <thangtc.work@gmail.com>
2022-06-28 22:30:09 +0700

    feat: add Head component

--- public/index.php
+++ public/index.php
@@ -1,1 +1,3 @@
-<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"><meta http-equiv="X-UA-Compatible" content="ie=edge"><title>Document</title><link rel="stylesheet" href="css/style.min.css"></head><body></body><script src="js/main.js"></script></html>
+<!doctype html><html lang="en"> <?php
+require "./partials/head.php"
+?> <body></body><script src="js/main.js"></script></html>
